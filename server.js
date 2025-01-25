20be8336a3e322b7982278d21e03e3d89350ab28
nsLittle <notsolittle88@gmail.com>
2025-01-25 17:18:20 -0500

    mongo fixed

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ mongoose
   .catch(err => console.error(err));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', index.html));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
