d25abde4fba80103ce206e808ac9ada8fa2b6089
nsLittle <notsolittle88@gmail.com>
2025-01-25 09:51:32 -0500

    updated mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const path = require('path');
 const cors = require('cors');
 
-mongoose.connect('mongodb://localhost/product-list');
+// mongoose.connect('mongodb://localhost/product-list');
+
+const mongoURI = process.env.MONGO_URI;
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
